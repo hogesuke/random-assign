7f988bab5e926671808f8a886fa9e4f730319ac1
hogesuke <miyado@gmail.com>
2018-11-04 00:18:28 +0900

    ランダムでアサインする機能を仮実装

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+  app.on('issue_comment.created', async context => {
+    if (context.isBot) { return; }
+
+    const contents = context.payload.comment.body.split(' ');
+    const command  = contents.shift();
+    const users    = contents;
+
+    if (!/^\/random/.test(command) && users.length <= 0) { return; }
+
+    const randomUser = users[Math.floor(Math.random() * users.length)]
+
+    console.log('randomUser', randomUser);
+
+    // const issueComment = context.issue({ body: 'New comment' })
+    // return context.github.issues.createComment(issueComment)
+
+    return context.github.issues.addAssigneesToIssue(context.issue({assignees: [randomUser]}));
   })
 
   // For more information on building apps:
