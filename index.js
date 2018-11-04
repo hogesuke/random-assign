f43c2a556588241179a6064b511b502c1be081d7
hogesuke <miyado@gmail.com>
2018-11-04 23:48:36 +0900

    ユーザ未指定時にはconfig.ymlからランダムに選ぶように修正

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
-
   app.on('issue_comment.created', async context => {
     if (context.isBot) { return; }
 
     const contents = context.payload.comment.body.split(' ');
-    const command  = contents.shift();
-    const users    = contents;
+    const command = contents.shift();
 
-    if (!/^\/random/.test(command) && users.length <= 0) { return; }
+    if (!/^\/random/.test(command)) { return; }
 
-    const randomUser = users[Math.floor(Math.random() * users.length)]
+    const config = await context.config('config.yml');
+    const maintainers = contents.length > 0 ? contents : config.maintainers;
 
-    console.log('randomUser', randomUser);
+    if (maintainers.length <= 0) { return; }
 
-    // const issueComment = context.issue({ body: 'New comment' })
-    // return context.github.issues.createComment(issueComment)
-
-    return context.github.issues.addAssigneesToIssue(context.issue({assignees: [randomUser]}));
+    const randomMaintainer = maintainers[Math.floor(Math.random() * maintainers.length)]
+    return context.github.issues.addAssigneesToIssue(context.issue({assignees: [randomMaintainer]}));
   })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
