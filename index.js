e2a3150f403dc929054441b3b5d552b788ef079e
hogesuke <miyado@gmail.com>
2018-11-05 23:19:36 +0900

    standard --fix

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 module.exports = app => {
   app.on('issue_comment.created', async context => {
-    if (context.isBot) { return; }
+    if (context.isBot) { return }
 
-    const contents = context.payload.comment.body.split(' ');
-    const command = contents.shift();
+    const contents = context.payload.comment.body.split(' ')
+    const command = contents.shift()
 
-    if (!/^\/random/.test(command)) { return; }
+    if (!/^\/random/.test(command)) { return }
 
-    const config = await context.config('config.yml');
-    const maintainers = contents.length > 0 ? contents : config.maintainers;
+    const config = await context.config('config.yml')
+    const maintainers = contents.length > 0 ? contents : config.maintainers
 
-    if (maintainers.length <= 0) { return; }
+    if (maintainers.length <= 0) { return }
 
     const randomMaintainer = maintainers[Math.floor(Math.random() * maintainers.length)]
-    return context.github.issues.addAssigneesToIssue(context.issue({assignees: [randomMaintainer]}));
+    return context.github.issues.addAssigneesToIssue(context.issue({assignees: [randomMaintainer]}))
   })
 }
